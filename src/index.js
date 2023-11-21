ea795ea6b4cbd62c2037085dba5b2dc3db23feca
Cintia Rivera <riveracintia67@gmail.com>
2023-11-21 19:36:50 -0300

    configuracion de express

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+  });

c14e5800f014cd46c76d61138a4d6fa2476c9da9
Cintia Rivera <riveracintia67@gmail.com>
2023-11-26 17:35:58 -0300

    configuracion de express

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import userRouter from "./routes/userRoutes.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +12,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
+app.use(userRouter);
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en el puerto ${PORT}`);

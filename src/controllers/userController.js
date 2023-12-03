aaf6a221d0e6957b29e524ab549c515effeead92
Cintia Rivera <riveracintia67@gmail.com>
2023-12-02 21:37:32 -0300

    agrego metodos y delete para users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,48 +1,112 @@
+import UserModel from '../models/userSchema.js';
+import bcrypt from 'bcryptjs';
 
-import UserModel from "../models/userSchema.js"
-import bcrypt from "bcryptjs"
-
-export const getUsers = async (_, res)=>{
-    try{
+export const getUsers = async (_, res) => {
+  try {
     const data = await UserModel.find({});
-    const filteredData = data.map((user)=>({
-        ... user._doc,
-        password:undefined,
-    }))
-    res.json({data:filteredData, message:"Usuarios encontrados"});} catch(e){
-console.error(e);}
-res.status(500).json({data:null,
-    message:"Ocurrio un error al conectarse a la DB",
-})}
+    const filteredData = data
+      .filter((user) => user._doc.isActive === true)
+      .map((user) => ({
+        ...user._doc,
+        password: undefined,
+        isActive:undefined,
+      }));
+    res.json({ data: filteredData, message: 'Usuarios encontrados' });
+  } catch (e) {
+    console.error(e);
+  }
+  res
+    .status(500)
+    .json({ data: null, message: 'Ocurrio un error al conectarse a la DB' });
+};
 
+export const postUser = async (req, res) => {
+  const { body } = req;
 
-export const postUser = async (req, res)=>{
-    const { body } = req;
+  const hashedPassword = bcrypt.hashSync(body.password, 10);
 
-    const hashedPassword = bcrypt.hashSync(body.password, 10);
+  const newUser = new UserModel({
+    firstname: body.firstname,
+    lastname: body.lastname,
+    username: body.username,
+    password: hashedPassword,
+    isActive: true,
+    isAdmin: false,
+  });
+  try {
+    await newUser.save();
 
-    const newUser = new UserModel({
-        firstname: body.firstname,
-        lastname: body.lastname,
-        username: body.username,
-        password: hashedPassword,
-        isActive:true,
-        isAdmin:false,
-    })
-    try{
-await newUser.save();
-
-res.status(201).json({data:null,
-    message: "Usuario creado exitosamente"
-});}catch(e){
-    if(e.message.includes("duplicate")){
-        res.status(400).json({data:null,
-            message: "El nombre de usuario ya esta en uso"
-        })
-        return;
+    res
+      .status(201)
+      .json({ data: null, message: 'Usuario creado exitosamente' });
+  } catch (e) {
+    if (e.message.includes('duplicate')) {
+      res
+        .status(400)
+        .json({ data: null, message: 'El nombre de usuario ya esta en uso' });
+      return;
     }
-    res.status(500).json({data:null,
-        message:"Ocurrio un error guardando el usuario"
+    res
+      .status(500)
+      .json({ data: null, message: 'Ocurrio un error guardando el usuario' });
+  }
+};
+
+export const putUser = async (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  try {
+    const action = await UserModel.updateOne({ _id: id }, body);
+    if (action.modifiedCount === 0) {
+      res.status(400).json({
+        data: null,
+        message: 'No se encontro un usuario con ese id',
+      });
+      return;
+    }
+
+    res.json({
+      data: null,
+      message: 'El usuario ha sido actualizado exitosamente',
     });
-}
-}
+  } catch (e) {
+    if (e.message.includes('duplicate')) {
+      res
+        .status(400)
+        .json({ data: null, message: 'El nombre de usuario ya esta en uso' });
+      return;
+    }
+    res.status(500).json({
+      data: null,
+      message: 'Ocurrio un error actualizando el usuario',
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const action = await UserModel.updateOne({ _id: id }, { isActive: false });
+    if (action.modifiedCount === 0) {
+      res.status(400).json({
+        data: null,
+        message: 'No se encontro un usuario con ese id',
+      });
+      return;
+    }
+
+    res.json({
+      data: null,
+      message: 'El usuario ha sido eliminado exitosamente',
+    });
+  } catch (e) {
+    res.status(500).json({
+      data: null,
+      message: 'Ocurrio un error eliminando el usuario',
+    });
+  }
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getUsers, postUser } from "../controllers/userController.js";
+import { getUsers, postUser, putUser, deleteUser} from "../controllers/userController.js";
 
 const router = express.Router(); 
 
 router.get("/", getUsers)
 
 router.post("/", postUser)
+
+router.put("/:id", putUser)
+
+router.delete("/:id", deleteUser)
 
 export default router;
